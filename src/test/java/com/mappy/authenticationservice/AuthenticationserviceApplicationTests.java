12c622f5031d9a206cf6d12738e2afc8690451fb
Ilias <i.elhammouti@student.fontys.nl>
2023-12-07 10:14:20 +0100

    Initial commit

---
+++ src/test/java/com/mappy/authenticationservice/AuthenticationserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mappy.authenticationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
